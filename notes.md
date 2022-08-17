2faa59ba6c987cb2f7b01af54a26cd7b974bc952
Abhilash Kakumanu <abhilashkakumanu1@gmail.com>
2022-08-17 11:05:49 +0530

    loop, vector

---
+++ notes.md
@@ -0,0 +1,1 @@
+`read_line` returns a result and we are using `expect` method on it to mimic `try..catch` syntax
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,20 @@ use std::io;
 fn main() {
     println!("Welcome to tasky - A simple todo application!");
 
-    // Read user input
-    println!("Enter input: ");
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("failed to read line.");
+    let mut tasks: Vec<String> = Vec::new();
 
-    println!("You entered: {}", input);
+    // Infinite loop => REPL
+    loop {
+        // Read user input
+        println!("Enter input: ");
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read line.");
+
+        tasks.push(input.trim().to_string());
+
+        println!("Tasks: {:?}", tasks);
+    }
 }
