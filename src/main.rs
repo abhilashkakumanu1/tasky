0eb58f7cdae8ed8492b2c396b3bed7faf6382e16
Abhilash Kakumanu <abhilashkakumanu1@gmail.com>
2022-08-18 11:03:15 +0530

    from vector, move to file based system

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
+use std::fs::{File, OpenOptions};
 use std::io;
+use std::io::Write;
+use std::path::Path;
 
 fn main() {
     println!("Welcome to tasky - A simple todo application!");
 
-    let mut tasks: Vec<String> = Vec::new();
+    let mut file: File;
+
+    // Check if the file exists, if not create it
+    let file_exists = Path::new("db.txt").exists();
+
+    if !file_exists {
+        file = File::create("db.txt").expect("Error creating the file.");
+    } else {
+        // If file already exists, open it in append mode
+        file = OpenOptions::new()
+            .append(true)
+            .open("db.txt")
+            .expect("Error opening the file.");
+    }
 
     // Infinite loop => REPL
     loop {
@@ -15,8 +31,7 @@ fn main() {
             .read_line(&mut input)
             .expect("failed to read line.");
 
-        tasks.push(input.trim().to_string());
-
-        println!("Tasks: {:?}", tasks);
+        file.write_all(input.to_owned().as_bytes())
+            .expect("Error writing to file.");
     }
 }
